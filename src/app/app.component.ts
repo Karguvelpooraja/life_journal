9c4f629b33d7076ec9c37bf7ede54cff781903e2
Karguvel Pooraja <karguvel.p-cnt@lntecc.com>
2025-09-25 14:21:46 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'karguvel';
+  title = 'Karguvel Portfolio';
+  currentYear: number = new Date().getFullYear();
+
+  aboutMe = {
+    name: 'Karguvel Pooraja',
+    currentRole: 'Senior Software Engineer at CIEL HR Services Pvt Ltd',
+    intro: 'I am passionate about software development, and building impactful digital solutions.'
+  };
+
+  education = [
+    {
+      school: 'SMA Govt Hr Sec School',
+      period: '2006 - 2013',
+      detail: 'Class 6 - 12 in Siruthondanallur, Tuticorin'
+    },
+    {
+      school: 'Kings Engineering College, Chennai',
+      period: '2013 - 2017',
+      detail: 'B.E (ECE), CGPA 7.3'
+    }
+  ];
+
+  experience = [
+    {
+      company: 'Avalon Technologies Pvt Ltd',
+      period: 'Aug 2017 - Mar 2021',
+      role: 'Quality Inspector',
+      detail: 'PCB board manufacturing company (MEPZ, Tambaram Sanatorium)'
+    },
+    {
+      company: 'Redim Information Systems Pvt Ltd',
+      period: 'Mar 2021 - Dec 2023',
+      role: 'UI Developer',
+      detail: 'Gaming development company (online casino games)'
+    },
+    {
+      company: 'Blind Matrix',
+      period: 'Dec 2023 - Mar 2024',
+      role: 'Software Engineer',
+      detail: 'Worked 3 months on software development projects'
+    },
+    {
+      company: 'CIEL HR Services Pvt Ltd',
+      period: 'Mar 2024 - Present',
+      role: 'Senior Software Engineer',
+      detail: 'Currently working as a software engineer'
+    }
+  ];
 }
